cfece5c53c04ac74dced802a450052b8f75bdcbb
arifamsar <dcylert@gmail.com>
2024-12-15 23:05:09 +0700

    feat: add CORS support and configure server host

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); 
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || 'localhost';
 
+app.use(cors()); 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -20,6 +23,6 @@ app.get("/", (req, res) => {
   res.send({ status: "mulai" })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
