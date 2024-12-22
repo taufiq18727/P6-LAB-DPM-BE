8b2c837de28c4cbb678de6f9d13d5b5bfa6bb483
arifamsar <dcylert@gmail.com>
2024-12-22 21:09:39 +0700

    feat: update app host configuration to allow external access

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(cors()); 
 app.use(express.json());
